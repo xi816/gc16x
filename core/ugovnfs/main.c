24e7dfba92117716efd83bbceb7f56ab608d05ed
xi816 <jej07267@gmail.com>
2024-12-06 16:12:29 +0300

    Added 42 new instructions

--- core/ugovnfs/main.c
+++ core/ugovnfs/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <holyc-types.h>
 
+U8 listPart(char* disk) {
+  if () {}
+  return 0;
+}
+
 // CLI tool to make GovnFS partitions
 U8 main(I32 argc, I8** argv) {
+  puts("UGovnFS 1.0");
+  if (argc == 1) {
+    puts("ugovnfs: no arguments given");
+    return 1;
+  }
+  FILE* fl = fopen(argv[2], "rb");
+  fseek(fl, 0, SEEK_END);
+  const U32 flsize = ftell(fl);
+  char disk[flsize];
+  fseek(fl, 0, SEEK_SET);
+  fread(disk, 1, flsize, fl);
+  fclose(fl);
+  if (!strcmp(argv[1], "-l")) {
+    return listPart(disk);
+  }
+  puts("ugovnfs: no arguments given");
   return 0;
 }
 
--- lib/cpu16/cpu16.h
+++ lib/cpu16/cpu16.h
@@ -1,20 +1,21 @@
 // CPU identificator: GC16X
-#include <cpu16/inst.h>
 #include <cpu16/proc/std.h>
 #define MEMSIZE 65536 // Maximum for a 16-bit cpu
 
 struct Regs {
-  U16 A;  // Accumulator
-  U16 B;  // Base
-  U16 C;  // Counter
-  U16 D;  // Data
-  U16 S;  // Segment (address)
-  U16 G;  // Segment #2 (address)
-  U8  H;  // High 8 bits
-  U8  L;  // Low 8 bits
-  U16 SP; // Stack pointer
-  U16 BP; // Base pointer
-  U16 PC; // Program counter
+  U16 A;   // Accumulator
+  U16 B;   // Base
+  U16 C;   // Counter
+  U16 D;   // Data
+  U16 S;   // Segment (address)
+  U16 G;   // Segment #2 (address)
+  U8  H;   // High 8 bits
+  U8  L;   // Low 8 bits
+  U8 STI; // Interrupt flag
+  U8 SCI; // Carry flag
+  U16 SP;  // Stack pointer
+  U16 BP;  // Base pointer
+  U16 PC;  // Program counter
 };
 struct RegClust {
   U8 x;
@@ -37,15 +38,15 @@ U16 ReadWord(GC gc, U16 addr) {
   return (gc.mem[addr]) + (gc.mem[addr+1] << 8);
 }
 
-U16* ReadReg(GC gc, U8 regid) {
+U16* ReadReg(GC* gc, U8 regid) {
   // PC register cannot be changed from
   // {REG} addressing instruction. It
   // can only be changed using JMP, CALL,
   // and other control flow instructions.
   U16* regids[10] = {
-    &(gc.r.A), &(gc.r.B), &(gc.r.C), &(gc.r.D),
-    &(gc.r.S), &(gc.r.G), (U16*)&(gc.r.H),
-    (U16*)&(gc.r.L), &(gc.r.SP), &(gc.r.BP)
+    &(gc->r.A), &(gc->r.B), &(gc->r.C), &(gc->r.D),
+    &(gc->r.S), &(gc->r.G), (U16*)&(gc->r.H),
+    (U16*)&(gc->r.L), &(gc->r.SP), &(gc->r.BP)
   };
   return regids[regid];
 }
@@ -64,7 +65,7 @@ U16 StackPop(GC* gc) {
 // Register clusters can only be used for reading data,
 // writing is not allowed.
 RegClust ReadRegClust(U8 clust) { // Read a register cluster
-  RegClust rc = {clust/12, clust%12};
+  RegClust rc = {clust/10, clust%10};
   return rc;
 }
 
@@ -80,7 +81,7 @@ U8 JMP0(GC* gc) {   // 0F 30
 }
 
 U8 JMP1(GC* gc) {   // 0F 31
-  gc->r.PC = *ReadReg(*gc, gc->r.PC+1);
+  gc->r.PC = *ReadReg(gc, gc->r.PC+1);
   return 0;
 }
 
@@ -96,7 +97,7 @@ U8 PUSH0(GC* gc) {  // 0F 84
 }
 
 U8 PUSH1(GC* gc) {  // 0F 90
-  StackPush(gc, *ReadReg(*gc, gc->mem[gc->r.PC+1]));
+  StackPush(gc, *ReadReg(gc, gc->mem[gc->r.PC+1]));
   gc->r.PC += 2;
   return 0;
 }
@@ -114,50 +115,30 @@ U8 CPUID(GC* gc) {  // 0F E9
   return 0;
 }
 
-U8 ADDA1(GC* gc) {  // 10 00
-  gc->r.A += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+U8 ADD11(GC* gc) {  // 10 00
+  RegClust rc = ReadRegClust(gc->mem[gc->r.PC+1]);
+  *ReadReg(gc, rc.x) += *ReadReg(gc, rc.y);
   gc->r.PC += 2;
   return 0;
 }
 
-U8 ADDB1(GC* gc) {  // 10 01
-  gc->r.B += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+U8 SUB11(GC* gc) {  // 10 01
+  RegClust rc = ReadRegClust(gc->mem[gc->r.PC+1]);
+  *ReadReg(gc, rc.x) -= *ReadReg(gc, rc.y);
   gc->r.PC += 2;
   return 0;
 }
 
-U8 ADDC1(GC* gc) {  // 10 02
-  gc->r.C += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+U8 MUL11(GC* gc) {  // 10 02
+  RegClust rc = ReadRegClust(gc->mem[gc->r.PC+1]);
+  *ReadReg(gc, rc.x) *= *ReadReg(gc, rc.y);
   gc->r.PC += 2;
   return 0;
 }
 
-U8 ADDD1(GC* gc) {  // 10 03
-  gc->r.D += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
-  gc->r.PC += 2;
-  return 0;
-}
-
-U8 ADDS1(GC* gc) {  // 10 04
-  gc->r.S += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
-  gc->r.PC += 2;
-  return 0;
-}
-
-U8 ADDG1(GC* gc) {  // 10 05
-  gc->r.G += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
-  gc->r.PC += 2;
-  return 0;
-}
-
-U8 ADDH1(GC* gc) {  // 10 06
-  gc->r.H += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
-  gc->r.PC += 2;
-  return 0;
-}
-
-U8 ADDL1(GC* gc) {  // 10 07
-  gc->r.L += *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+U8 DIV11(GC* gc) {  // 10 03
+  RegClust rc = ReadRegClust(gc->mem[gc->r.PC+1]);
+  *ReadReg(gc, rc.x) /= *ReadReg(gc, rc.y);
   gc->r.PC += 2;
   return 0;
 }
@@ -199,26 +180,170 @@ U8 ADDG0(GC* gc) {  // 10 0D
 }
 
 U8 ADDH0(GC* gc) {  // 10 0E
-  gc->r.A += ReadWord(*gc, gc->r.PC+1);
+  gc->r.H += ReadWord(*gc, gc->r.PC+1);
   gc->r.PC += 3;
   return 0;
 }
 
 U8 ADDL0(GC* gc) {  // 10 0F
-  gc->r.A += ReadWord(*gc, gc->r.PC+1);
+  gc->r.L += ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBA0(GC* gc) {  // 10 18
+  gc->r.A -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBB0(GC* gc) {  // 10 19
+  gc->r.B -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBC0(GC* gc) {  // 10 1A
+  gc->r.C += ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBD0(GC* gc) {  // 10 1B
+  gc->r.D -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBS0(GC* gc) {  // 10 1C
+  gc->r.S -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBG0(GC* gc) {  // 10 1D
+  gc->r.G -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBH0(GC* gc) {  // 10 1E
+  gc->r.H -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 SUBL0(GC* gc) {  // 10 1F
+  gc->r.L -= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULA0(GC* gc) {  // 10 28
+  gc->r.A *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULB0(GC* gc) {  // 10 29
+  gc->r.B *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULC0(GC* gc) {  // 10 2A
+  gc->r.C *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULD0(GC* gc) {  // 10 2B
+  gc->r.D *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULS0(GC* gc) {  // 10 2C
+  gc->r.S *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULG0(GC* gc) {  // 10 2D
+  gc->r.G *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULH0(GC* gc) {  // 10 2E
+  gc->r.H *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 MULL0(GC* gc) {  // 10 2F
+  gc->r.L *= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVA0(GC* gc) {  // 10 38
+  gc->r.A /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVB0(GC* gc) {  // 10 39
+  gc->r.B /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVC0(GC* gc) {  // 10 3A
+  gc->r.C /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVD0(GC* gc) {  // 10 3B
+  gc->r.D /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVS0(GC* gc) {  // 10 3C
+  gc->r.S /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVG0(GC* gc) {  // 10 3D
+  gc->r.G /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVH0(GC* gc) {  // 10 3E
+  gc->r.H /= ReadWord(*gc, gc->r.PC+1);
+  gc->r.PC += 3;
+  return 0;
+}
+
+U8 DIVL0(GC* gc) {  // 10 3F
+  gc->r.L /= ReadWord(*gc, gc->r.PC+1);
   gc->r.PC += 3;
   return 0;
 }
 
 U8 LODSB(GC* gc) {  // 10 87
   gc->r.S = ReadByte(*gc, gc->r.S);
-  gc->r.PC += 1;
+  gc->r.PC++;
   return 0;
 }
 
 U8 LODGB(GC* gc) {  // 10 88
   gc->r.G = ReadByte(*gc, gc->r.G);
-  gc->r.PC += 1;
+  gc->r.PC++;
   return 0;
 }
 
@@ -230,6 +355,116 @@ U8 STOSB(GC* gc) {  // 10 89
 
 U8 STOGB(GC* gc) {  // 10 8A
   gc->mem[gc->r.G] = ReadByte(*gc, gc->r.PC+1);
+  gc->r.PC += 2;
+  return 0;
+}
+
+U8 INXA(GC* gc) {   // 10 C0
+  gc->r.A++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXB(GC* gc) {   // 10 C1
+  gc->r.B++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXC(GC* gc) {   // 10 C2
+  gc->r.C++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXD(GC* gc) {   // 10 C3
+  gc->r.D++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXS(GC* gc) {   // 10 C4
+  gc->r.S++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXG(GC* gc) {   // 10 C5
+  gc->r.G++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXH(GC* gc) {   // 10 C6
+  gc->r.H++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 INXL(GC* gc) {   // 10 C7
+  gc->r.L++;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXA(GC* gc) {   // 10 D0
+  gc->r.A--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXB(GC* gc) {   // 10 D1
+  gc->r.B--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXC(GC* gc) {   // 10 D2
+  gc->r.C--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXD(GC* gc) {   // 10 D3
+  gc->r.D--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXS(GC* gc) {   // 10 D4
+  gc->r.S--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXG(GC* gc) {   // 10 D5
+  gc->r.G--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXH(GC* gc) {   // 10 D6
+  gc->r.H--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 DEXL(GC* gc) {   // 10 D7
+  gc->r.L--;
+  gc->r.PC++;
+  return 0;
+}
+
+U8 AND11(GC* gc) {  // 10 D8
+  RegClust rc = ReadRegClust(gc->mem[gc->r.PC+1]);
+  *ReadReg(gc, rc.x) &= *ReadReg(gc, rc.y);
+  gc->r.PC += 2;
+  return 0;
+}
+
+U8 OR11(GC* gc) {  // 10 D9
+  RegClust rc = ReadRegClust(gc->mem[gc->r.PC+1]);
+  *ReadReg(gc, rc.x) |= *ReadReg(gc, rc.y);
   gc->r.PC += 2;
   return 0;
 }
@@ -331,58 +566,58 @@ U8 LDL2(GC* gc) {   // 66 99
 }
 
 U8 LDA1(GC* gc) {   // 66 41
-  gc->r.A = *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+  gc->r.A = *ReadReg(gc, gc->mem[gc->r.PC+1]);
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDB1(GC* gc) {   // 66 42
-  gc->r.B = *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+  gc->r.B = *ReadReg(gc, gc->mem[gc->r.PC+1]);
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDC1(GC* gc) {   // 66 43
-  gc->r.C = *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+  gc->r.C = *ReadReg(gc, gc->mem[gc->r.PC+1]);
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDD1(GC* gc) {   // 66 44
-  gc->r.D = *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+  gc->r.D = *ReadReg(gc, gc->mem[gc->r.PC+1]);
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDS1(GC* gc) {   // 66 45
-  gc->r.S = *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+  gc->r.S = *ReadReg(gc, gc->mem[gc->r.PC+1]);
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDG1(GC* gc) {   // 66 46
-  gc->r.G = *ReadReg(*gc, gc->mem[gc->r.PC+1]);
+  gc->r.G = *ReadReg(gc, gc->mem[gc->r.PC+1]);
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDH1(GC* gc) {   // 66 47
-  gc->r.H = *ReadReg(*gc, gc->mem[gc->r.PC+1]) % 256;
+  gc->r.H = *ReadReg(gc, gc->mem[gc->r.PC+1]) % 256;
   gc->r.PC += 2;
   return 0;
 }
 
 U8 LDL1(GC* gc) {   // 66 48
-  gc->r.L = *ReadReg(*gc, gc->mem[gc->r.PC+1]) % 256;
+  gc->r.L = *ReadReg(gc, gc->mem[gc->r.PC+1]) % 256;
   gc->r.PC += 2;
   return 0;
 }
 
-U8 INT(GC* gc, bool ri) {   // C2
+U8 INT(GC* gc, bool ri) {
+  if (gc->r.STI) return 0;
   char val;
   if (ri) {
-    val = *ReadReg(*gc, ReadByte(*gc, gc->r.PC+1));
-    printf("Register interrupt: %02X\n", val);
+    val = *ReadReg(gc, ReadByte(*gc, gc->r.PC+1));
   }
   else {
     val = ReadByte(*gc, gc->r.PC+1);
@@ -397,22 +632,22 @@ U8 INT(GC* gc, bool ri) {   // C2
       break;
     }
     default:
-      printf("Unknown interrupt %02X\n", val);
+      printf("Illegal interrupt %02X\n", val);
       return 1;
   }
   gc->r.PC += 2;
   return 0;
 }
 
-U8 INT0(GC* gc) {
+U8 INT0(GC* gc) { // 0F C2
   return INT(gc, false);
 }
 
-U8 INT1(GC* gc) {
+U8 INT1(GC* gc) { // 0F C3
   return INT(gc, true);
 }
 
-U8 NOP(GC* gc) {    // EA
+U8 NOP(GC* gc) { // EA
   return 0;
 }
 
@@ -460,10 +695,10 @@ U8 (*INSTS_PG0F[256])() = {
 };
 
 U8 (*INSTS_PG10[256])() = {
-  &ADDA1, &ADDB1, &ADDC1, &ADDD1, &ADDS1, &ADDG1, &ADDH1, &ADDL1, &ADDA0, &ADDB0, &ADDC0, &ADDD0, &ADDS0, &ADDG0, &ADDH0, &ADDL0,
-  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
-  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
-  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
+  &ADD11, &SUB11, &MUL11, &DIV11, &UNK  , &UNK  , &UNK  , &UNK  , &ADDA0, &ADDB0, &ADDC0, &ADDD0, &ADDS0, &ADDG0, &ADDH0, &ADDL0,
+  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &SUBA0, &SUBB0, &SUBC0, &SUBD0, &SUBS0, &SUBG0, &SUBH0, &SUBL0,
+  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &MULA0, &MULB0, &MULC0, &MULD0, &MULS0, &MULG0, &MULH0, &MULL0,
+  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &DIVA0, &DIVB0, &DIVC0, &DIVD0, &DIVS0, &DIVG0, &DIVH0, &DIVL0,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
@@ -472,8 +707,8 @@ U8 (*INSTS_PG10[256])() = {
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
-  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
-  &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
+  &INXA , &INXB , &INXC , &INXD , &INXS , &INXG , &INXH , &INXL , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
+  &DEXA , &DEXB , &DEXC , &DEXD , &DEXS , &DEXG , &DEXH , &DEXL , &AND11, &OR11 , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  ,
   &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK  , &UNK
 };
@@ -516,6 +751,9 @@ U0 Reset(GC* gc) {
   gc->r.SP = 0x1000;
   gc->r.BP = 0x1000;
   gc->r.PC = 0x0000;
+  
+  gc->r.STI = 0x00;
+  gc->r.SCI = 0x00;
 }
 
 U0 StackDump(GC gc) {
