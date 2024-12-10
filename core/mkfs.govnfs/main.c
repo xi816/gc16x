4b5cf9be2f0e89b89d30513df91ac41e23a72179
xi816 <jej07267@gmail.com>
2024-12-10 22:58:26 +0300

    - ugovnfs update and ball build system usage

--- core/mkfs.govnfs/main.c
+++ core/mkfs.govnfs/main.c
@@ -14,7 +14,14 @@
 #define I32 int
 #define I64 long long int
 
-I8 GFS_HEADER_FULL[] = "\x89GOVNFS1.0  \x12\x34\xA4\x20\x7F"; // 7F is File Table End
+// Header defining the GovnFS version
+U8 GFS_HEADER_FULL[]   = "\x89GOVNFS1.2  \x14\x9E\xA1\x55";
+// First disk partition
+// Name: /boot/
+// ID: 0000h
+// Size: 4KB
+U8 GFS_SIZE[]          = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\xFF\xFF\xFF\xF9/boot/     \x00\x00";
+U8 GFS_BOOT_PART_END[] = "\xFF\xFF\xFF\xF9\xFF\xFF\xFF\xF4";
 
 U0 blocksIn(U16 blocks) {
   printf("%d blocks read\n", blocks);
@@ -24,8 +31,15 @@ U0 blocksOut(U16 blocks) {
   printf("%d blocks written\n", blocks);
 }
 
-U16 writeHeader(U8* buf, U16 startAddr) {
+U16 writeHeader(U8* buf, U16 startAddr, U32 diskSize) {
   memcpy(buf+startAddr, GFS_HEADER_FULL, sizeof(GFS_HEADER_FULL));
+  GFS_SIZE[0] = diskSize / 256;
+  GFS_SIZE[1] = diskSize % 256; // 2 bytes for disk size and 14 bytes reserved
+  memcpy(buf+startAddr+16, GFS_SIZE, sizeof(GFS_SIZE));
+}
+
+U16 writeBootPartitionEnd(U8* buf, U16 startAddr) {
+  memcpy(buf+startAddr, GFS_BOOT_PART_END, sizeof(GFS_BOOT_PART_END));
 }
 
 I32 main(I32 argc, I8** argv) {
@@ -51,10 +65,11 @@ I32 main(I32 argc, I8** argv) {
   fread(drvbuf, 1, drvlen, drvfile);
   blocksIn(blocks);
 
-  writeHeader(drvbuf, startInd);
-  for (U32 bus = startInd+sizeof(GFS_HEADER_FULL); bus < sizeof(drvbuf); bus++) {
-    drvbuf[bus] = 0;
+  writeHeader(drvbuf, startInd, blocks);
+  for (U32 bus = 0; bus < 4096; bus++) {
+    drvbuf[bus+startInd+49] = 0;
   }
+  writeBootPartitionEnd(drvbuf, 4096+startInd+49);
 
   fseek(drvfile, 0, SEEK_SET);
   U16 blockso = fwrite(drvbuf, 1, sizeof(drvbuf), drvfile)/512;
@@ -63,3 +78,4 @@ I32 main(I32 argc, I8** argv) {
 
   return 0;
 }
+
--- core/ugovnfs/main.c
+++ core/ugovnfs/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
 #include <holyc-types.h>
 
-U8 listPart(char* disk) {
-  if () {}
+// The header is the first 32 bytes of the disk
+U8 readHeader(U8* disk) {
+  puts("Disk info:");
+  printf("  Magic byte: %02X\n", disk[0x00]);
+  printf("  Filesystem: \"%c%c%c%c%c%c%c%c%c%c%c\"\n",
+    disk[0x01], disk[0x02], disk[0x03], disk[0x04], disk[0x05], disk[0x06],
+    disk[0x07], disk[0x08], disk[0x09], disk[0x0A], disk[0x0B]);
+  printf("  Serial number: %02X%02X%02X%02X\n", disk[0x0C], disk[0x0D], disk[0x0E], disk[0x0F]);
+  printf("  Disk size (sectors): %02X%02X\n", disk[0x10], disk[0x11]);
   return 0;
 }
 
 // CLI tool to make GovnFS partitions
 U8 main(I32 argc, I8** argv) {
-  puts("UGovnFS 1.0");
+  // puts("UGovnFS 1.0");
   if (argc == 1) {
     puts("ugovnfs: no arguments given");
     return 1;
@@ -20,8 +29,8 @@ U8 main(I32 argc, I8** argv) {
   fseek(fl, 0, SEEK_SET);
   fread(disk, 1, flsize, fl);
   fclose(fl);
-  if (!strcmp(argv[1], "-l")) {
-    return listPart(disk);
+  if (!strcmp(argv[1], "-i")) {
+    return readHeader(disk);
   }
   puts("ugovnfs: no arguments given");
   return 0;
