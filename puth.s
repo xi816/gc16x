cf569f8ea0b12619c96db05ea47cc40f09fda08e
xi816 <jej07267@gmail.com>
2025-01-16 09:09:38 +0300

    - Know

--- puth.s
+++ puth.s
@@ -1,22 +1,45 @@
-jmp main
-puth:
-  cmp %a $00
-  re
+call start
+  push $00
+  int $00
+puthc:
   div %a #16
+  ldg hex-numbers
+  add %g %a
+  lodgb
+  push %g
+  int $02
   ldg hex-numbers
   add %g %d
   lodgb
   push %g
   int $02
-  jmp puth
+  ret
 hex-numbers: bytes "0123456789ABCDEF"
 
+start:
+  lds $0000
 main:
-  lda $4A69
-  call puth
+  ldds
+  call puthc
 
-  push $0A
-  int $00
+  push $20
+  int $02
+  inx %s
+
+  ldb %s
+  div %s #32
+  lds %b
+  cmp %d $00
+  jme enter
+
+  ; int $01
+  ; pop %b
+  jmp main
+
   push $00
   int $00
+enter:
+  push $0A
+  int $02
+  jmp main
 
