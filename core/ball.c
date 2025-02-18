e20a7a87db28631534fb97e49e793c193edbe753
xi816 <jej07267@gmail.com>
2025-02-18 21:15:26 +0300

    Optimized ball

--- core/ball.c
+++ core/ball.c
@@ -12,23 +12,21 @@ int32_t main(void) {
     color = "\0";
     rcolor = "\0";
   }
-  char* targets[] = {"prepare-disk", "gc16", "gasman", "gboot", "ugovnfs"};
-  char* builddirs[] = {"core/", "./", "core/gasman", "core/gboot", "core/ugovnfs"};
+  char* targets[] = {"ball", "prepare-disk", "gc16", "gasman", "gboot", "ugovnfs"};
   char* commands[] = {
-    "gcc prepare-disk.c -o ../prepare-disk",
+    "gcc core/ball.c -o ball",
+    "gcc core/prepare-disk.c -o prepare-disk",
     "gcc core/main.c -Ilib -lSDL2 -o gc16",
-    "gcc main.c -I../../lib -o ../../gasman",
-    "gcc main.c -o ../../gboot",
-    "gcc main.c -I../../lib -o ../../ugovnfs"
+    "gcc core/gasman/main.c -Ilib -o gasman",
+    "gcc core/gboot/main.c -o gboot",
+    "gcc core/ugovnfs/main.c -Ilib -o ugovnfs"
   };
 
-  char fcom[256];
   printf("rebuilding %sball%s\n", color, rcolor);
   system("gcc core/ball.c -o ball");
   for (uint16_t i = 0; i < ptrlen(targets); i++) {
     printf("building %s%s%s\n", color, targets[i], rcolor);
-    sprintf(fcom, "cd %s; %s; cd - > /dev/null", builddirs[i], commands[i]);
-    system(fcom);
+    system(commands[i]);
   }
   return 0;
 }
