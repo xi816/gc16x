fd6db3858dec9039275e842004b66c971d003870
xi816 <jej07267@gmail.com>
2025-02-18 21:00:51 +0300

    Remove every Bash script

--- core/ball.c
+++ core/ball.c
@@ -14,12 +14,20 @@ int32_t main(void) {
   }
   char* targets[] = {"prepare-disk", "gc16", "gasman", "gboot", "ugovnfs"};
   char* builddirs[] = {"core/", "./", "core/gasman", "core/gboot", "core/ugovnfs"};
+  char* commands[] = {
+    "gcc prepare-disk.c -o ../prepare-disk",
+    "gcc core/main.c -Ilib -lSDL2 -o gc16",
+    "gcc main.c -I../../lib -o ../../gasman",
+    "gcc main.c -o ../../gboot",
+    "gcc main.c -I../../lib -o ../../ugovnfs"
+  };
+
   char fcom[256];
   printf("rebuilding %sball%s\n", color, rcolor);
   system("gcc core/ball.c -o ball");
   for (uint16_t i = 0; i < ptrlen(targets); i++) {
     printf("building %s%s%s\n", color, targets[i], rcolor);
-    sprintf(fcom, "cd %s; ./build; cd - > /dev/null", builddirs[i]);
+    sprintf(fcom, "cd %s; %s; cd - > /dev/null", builddirs[i], commands[i]);
     system(fcom);
   }
   return 0;
